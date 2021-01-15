1c7ef2067ffdaad6e42e0b320ec54befdc696ad5
angie <angheloalf95@gmail.com>
2021-01-14 22:00:31 -0300

    void func_800E66A0(void)
    Signed-off-by: angie <angheloalf95@gmail.com>

--- src/code/code_800E11F0.c
+++ src/code/code_800E11F0.c
@@ -289,7 +289,11 @@ s8 *func_800E27F8(s32 arg0) {
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/code_800E11F0/func_800E6680.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_800E11F0/func_800E66A0.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/code/code_800E11F0/func_800E66A0.s")
+
+void func_800E66A0(void) {
+    func_800E66C0(2);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/code_800E11F0/func_800E66C0.s")
 
