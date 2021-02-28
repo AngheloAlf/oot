d935c95e8677e4d9ec5da12f1dd0e7b2167655e8
angie <angheloalf95@gmail.com>
2021-02-28 18:19:14 -0300

    Add csv export option to comparison script
    Signed-off-by: angie <angheloalf95@gmail.com>

--- log_make.sh
+++ log_make.sh
@@ -1,32 +1,36 @@
-echo "> make clean clean_assets"
+echo "$ make clean clean_assets"
 make clean clean_assets
 
 echo ""
-echo "> python3 extract_baserom.py > logs/log_extract_baserom_pal_mq.txt 2>&1"
+echo "$ python3 extract_baserom.py"
 python3 extract_baserom.py > logs/log_extract_baserom_pal_mq.txt 2>&1
 
 echo ""
-echo "> python3 extract_assets.py > logs/log_extract_assets_pal_mq.txt 2>&1"
+echo "$ python3 extract_assets.py"
 python3 extract_assets.py > logs/log_extract_assets_pal_mq.txt 2>&1
 
 echo ""
-echo "> python3 tools/compare_extracted_baseroms.py ../oot_master/baserom > logs/log_compare_extracted_baseroms_pal_mq_vs_pal_mq_dbg.txt 2>&1"
+echo "$ python3 tools/compare_extracted_baseroms.py ../oot_master/baserom"
 python3 tools/compare_extracted_baseroms.py ../oot_master/baserom > logs/log_compare_extracted_baseroms_pal_mq_vs_pal_mq_dbg.txt 2>&1
+python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --csv > logs/log_compare_extracted_baseroms_pal_mq_vs_pal_mq_dbg.csv
 
 echo ""
-echo "> python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --print equals > logs/log_compare_extracted_baseroms_equals_pal_mq_vs_pal_mq_dbg.txt 2>&1"
+echo "$ python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --print equals"
 python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --print equals > logs/log_compare_extracted_baseroms_equals_pal_mq_vs_pal_mq_dbg.txt 2>&1
+python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --print equals --csv > logs/log_compare_extracted_baseroms_equals_pal_mq_vs_pal_mq_dbg.csv
 
 echo ""
-echo "> python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --print diffs > logs/log_compare_extracted_baseroms_diffs_pal_mq_vs_pal_mq_dbg.txt 2>&1"
+echo "$ python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --print diffs"
 python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --print diffs > logs/log_compare_extracted_baseroms_diffs_pal_mq_vs_pal_mq_dbg.txt 2>&1
+python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --print diffs --csv > logs/log_compare_extracted_baseroms_diffs_pal_mq_vs_pal_mq_dbg.csv
 
 echo ""
-echo "> python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --print missing > logs/log_compare_extracted_baseroms_missing_pal_mq_vs_pal_mq_dbg.txt 2>&1"
+echo "$ python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --print missing"
 python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --print missing > logs/log_compare_extracted_baseroms_missing_pal_mq_vs_pal_mq_dbg.txt 2>&1
+python3 tools/compare_extracted_baseroms.py ../oot_master/baserom --print missing --csv > logs/log_compare_extracted_baseroms_missing_pal_mq_vs_pal_mq_dbg.csv
 
 echo ""
-echo "> make all > logs/log_make_all_pal_mq.txt 2>&1"
+echo "$ make all"
 make all > logs/log_make_all_pal_mq.txt 2>&1
 
 echo ""
--- tools/compare_extracted_baseroms.py
+++ tools/compare_extracted_baseroms.py
@@ -18,24 +18,13 @@ def read_file_as_bytearray(filepath):
     with open(filepath, mode="rb") as f:
         return bytearray(f.read())
 
-def compare_files(filename, filepath_one, filepath_two, print_type):
+def compare_files(filepath_one, filepath_two):
     file_one = read_file_as_bytearray(filepath_one)
     file_two = read_file_as_bytearray(filepath_two)
     are_equal = get_str_hash(file_one) == get_str_hash(file_two)
-    if are_equal and print_type in ("all", "equals"):
-        print(f"{filename} OK")
-    if not are_equal and print_type in ("all", "diffs"):
-        print(f"{filename} not OK")
-        len_one = len(file_one)
-        len_two = len(file_two)
-        if len_one != len_two:
-            div = 0
-            if len_two != 0:
-                div = round(len_one/len_two, 3)
-            print(f"\tSize doesn't match: {len_one} vs {len_two} (x{div}) ({len_one-len_two})")
-        else:
-            print("\tSize matches.")
-    return are_equal
+    len_one = len(file_one)
+    len_two = len(file_two)
+    return are_equal, len_one, len_two
 
 
 def compare_baseroms(second_baserom_path, print_type):
@@ -58,16 +47,28 @@ def compare_baseroms(second_baserom_path, print_type):
                 print(f"File {filename} in baserom does not exists in other_baserom")
             continue
         
-        if not compare_files(filename, filepath_one, filepath_two, print_type):
+        are_equal, len_one, len_two = compare_files(filepath_one, filepath_two)
+        if not are_equal:
             different += 1
+            if print_type in ("all", "diffs"):
+                print(f"{filename} not OK")
+                if len_one != len_two:
+                    div = 0
+                    if len_two != 0:
+                        div = round(len_one/len_two, 3)
+                    print(f"\tSize doesn't match: {len_one} vs {len_two} (x{div}) ({len_one-len_two})")
+                else:
+                    print("\tSize matches.")
         else:
             equals += 1
-    
-    for filename in os.listdir(second_baserom_path):
-        files_baserom_two.add(filename)
-        if filename not in files_baserom_one:
-            missing_in_one.add(filename)
-            if print_type in ("all", "missing"):
+            if print_type in ("all", "equals"):
+                print(f"{filename} OK")
+
+    if print_type in ("all", "missing"):
+        for filename in os.listdir(second_baserom_path):
+            files_baserom_two.add(filename)
+            if filename not in files_baserom_one:
+                missing_in_one.add(filename)
                 print(f"File {filename} from other_baserom does not exists in baserom")
 
     total = len(files_baserom_one)
@@ -83,6 +84,39 @@ def compare_baseroms(second_baserom_path, print_type):
             print(f"Extras:     {len(missing_in_one)}")
 
 
+def compare_to_csv(second_baserom_path, print_type):
+    files_baserom_one = set()
+
+    print("File,Are equals,Size in baserom,Size in other_baserom,Size proportion,Size difference")
+
+    for filename in os.listdir(baserom_path):
+        files_baserom_one.add(filename)
+        filepath_one = os.path.join(baserom_path, filename)
+        filepath_two = os.path.join(second_baserom_path, filename)
+        if not os.path.exists(filepath_two):
+            if print_type in ("all", "missing"):
+                len_one = len(read_file_as_bytearray(filepath_one))
+                print(f"{filename},,{len_one},,,")
+            continue
+        
+        are_equal, len_one, len_two = compare_files(filepath_one, filepath_two)
+        div = 0
+        if len_two != 0:
+            div = round(len_one/len_two, 3)
+        if are_equal and print_type not in ("all", "equals"):
+            continue
+        if not are_equal and print_type not in ("all", "diffs"):
+            continue
+        print(f"{filename},{are_equal},{len_one},{len_two},{div},{len_one-len_two}")
+
+    if print_type in ("all", "missing"):
+        for filename in os.listdir(second_baserom_path):
+            filepath_two = os.path.join(second_baserom_path, filename)
+            if filename not in files_baserom_one:
+                len_two = len(read_file_as_bytearray(filepath_two))
+                print(f"{filename},,,{len_two},,")
+
+
 def main():
     description = ""
 
@@ -91,9 +125,13 @@ def main():
     parser = argparse.ArgumentParser(description=description, epilog=epilog, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("other_baserom", help="Path to the baserom folder that will be compared against.")
     parser.add_argument("--print", help="Select what will be printed for a cleaner output. Default is 'all'.", choices=["all", "equals", "diffs", "missing"], default="all")
+    parser.add_argument("--csv", help="Print the output in csv format instead.", action="store_true")
     args = parser.parse_args()
 
-    compare_baseroms(args.other_baserom, args.print)
+    if args.csv:
+        compare_to_csv(args.other_baserom, args.print)
+    else:
+        compare_baseroms(args.other_baserom, args.print)
 
 
 if __name__ == "__main__":
